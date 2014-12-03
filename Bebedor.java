ea8da0e05754af769dcc12be10c6c78acdb1ce1d
MJGM <MJGM@example.com>
2014-12-03 12:56:33 +0100

    metodo de bebedor que pide copas

--- Bebedor.java
+++ Bebedor.java
@@ -9,6 +9,7 @@ public class Bebedor
 {
     // instance variables - replace the example below with your own
     private float alcoholEnSangre;
+    private int   nivelMaximoAlcohol;
 
     /**
      * Constructor for objects of class Bebedor con nivel de alcohol en sangre cero
@@ -16,12 +17,25 @@ public class Bebedor
     public Bebedor()
     {
         this.alcoholEnSangre = 0;
+        this.nivelMaximoAlcohol = 32;
 
     }
     
+    /**
+     * retorna el nivel de alcohol en sangre
+     */
     public float getAlcoholEnSangre()
     {
         return alcoholEnSangre;
+    }
+    
+    /**
+     * bebe una copa
+     * e incrementa el nivel de alcohol en sangre según la copa
+     */
+    public void bebeCopa(Cubata nombreCopa)
+    {
+        alcoholEnSangre = alcoholEnSangre + nombreCopa.getPorAlcohol();
     }
     
     
